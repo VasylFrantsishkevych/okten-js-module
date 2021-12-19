98401fc90a3fc72e2379f5fe20be0f8a5e1e181b
VasylFrantsishkevych <vasul.fr@gmail.com>
2021-12-19 13:39:04 +0200

    mini

---
+++ mini-project/mini.js
@@ -0,0 +1,45 @@
+// В index.html
+// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
+// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html,
+// которая имеет детальную информацию про объект на который кликнули
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => {
+
+        let divContainerUser = document.createElement('div');
+        divContainerUser.classList.add('user__name_container');
+        document.body.appendChild(divContainerUser);
+
+        for (const user of users) {
+
+            let divUserName = document.createElement('div');
+            divUserName.classList.add('user__name_card');
+            divUserName.innerHTML = `
+                                    <h2>Id: ${user.id}</h2>
+                                    <h2>${user.name}</h2>
+                                    `;
+            let aUser = document.createElement('a');
+            aUser.innerText = 'User Details';
+            aUser.href =  'user-details.html?id='+user.id;
+
+            divUserName.appendChild(aUser);
+            divContainerUser.appendChild(divUserName);
+        };
+});
+
+
+//
+// На странице post-details.html:
+// 7 Вывести всю, без исключения, информацию про объект post на кнопку/ссылку которого был совершен клик ранее.
+// 8 Ниже информации про пост, вывести все комментарии текущего поста (эндпоинт для получения информации -
+// https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+//
+// Стилизация проекта -
+// index.html - все блоки с user - по 2 в ряд. кнопки/ссылки находяться под информацией про user.
+// user-details.html - блок с информацией про user вверху страницы. Кнопка ниже, на 90% ширины страницы, по центру.
+// блоки с краткой информацией про post - в ряд по 5 объектов.
+// post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
+// Все без исключения элементы, который характеризируют user,post,comment  визуализировать, так,
+// что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
---
+++ mini-project/user-details.js
@@ -0,0 +1,120 @@
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html,
+// которая имеет детальную информацию про текущий пост.
+
+let url = new URL(location.href);
+let idOfUsers = url.searchParams.get('id');
+fetch('https://jsonplaceholder.typicode.com/users/' + idOfUsers)
+    .then(response => response.json())
+    .then(users => {
+
+        let divUserContainer = document.createElement('div');
+        divUserContainer.classList.add('container');
+
+        let divUserTop = document.createElement('div');
+        divUserTop.classList.add('user__top');
+
+        let divUserTopLeft = document.createElement('div');
+        divUserTopLeft.classList.add('user__top_info');
+        divUserTopLeft.innerHTML = `
+                                    <h3>username: ${users.username}</h3>
+                                    <h3>phone: ${users.phone}</h3>
+                                    `;
+
+        let divUserTopRight = document.createElement('div');
+        divUserTopRight.classList.add('user__top_info');
+        divUserTopRight.innerHTML = `
+                                     <h3>email: ${users.email}</h3>
+                                     <h3>website: ${users.website}</h3>
+                                     `;
+
+        let divUserBottom = document.createElement('div');
+        divUserBottom.classList.add('user__bottom');
+
+        let divBottomAddress = document.createElement('div');
+        divBottomAddress.classList.add('user__bottom_ul')
+
+        let divBottomGeo = document.createElement('div');
+        divBottomGeo.classList.add('user__bottom_ul');
+
+        let ulAddress = document.createElement('ul');
+        ulAddress.innerHTML = '<strong>Address</strong>';
+
+        for (const addressKey in users.address) {
+            if (addressKey !== 'geo') {
+                let liAddress = document.createElement('li');
+                liAddress.innerText = `${addressKey}: ${users.address[addressKey]}`;
+                ulAddress.appendChild(liAddress);
+            } else {
+                let ulGeo = document.createElement('ul');
+                ulGeo.innerHTML = '<strong>Geo</strong>';
+                divBottomGeo.appendChild(ulGeo);
+
+                for (const geoKey in users.address.geo) {
+                    let liGeo = document.createElement('li');
+                    liGeo.innerText = `${geoKey}: ${users.address.geo[geoKey]}`;
+                    ulGeo.appendChild(liGeo);
+                }
+            }
+        }
+
+        let divBottomCompany = document.createElement('div');
+        divBottomCompany.classList.add('user__bottom_ul');
+
+        let ulCompany = document.createElement('ul');
+        ulCompany.innerHTML = '<strong>Company</strong>';
+        divBottomCompany.appendChild(ulCompany);
+
+        for (const companyKey in users.company) {
+            let liCompany = document.createElement('li');
+            liCompany.innerText = `${companyKey}: ${users.company[companyKey]}`;
+            ulCompany.appendChild(liCompany);
+        }
+
+        let divPost = document.createElement('div');
+        divPost.classList.add('posts')
+
+        let divUserButton = document.createElement('div');
+        divUserButton.classList.add('user__button');
+
+        let btnUsers = document.createElement('button');
+        btnUsers.innerText = 'post of current user';
+
+        btnUsers.onclick = function () {
+            divPostTitle.classList.toggle('post__title_open');
+        }
+
+        let divPostTitle = document.createElement('div');
+        divPostTitle.classList.add('post__title');
+
+        fetch(`https://jsonplaceholder.typicode.com/users/${users.id}/posts`)
+            .then((response) => response.json())
+            .then((posts) => {
+                posts.forEach(post => {
+                    let divPostCard = document.createElement('div');
+                    divPostCard.classList.add('post__title_card');
+
+                    let pPostTitle = document.createElement('p');
+                    pPostTitle.innerText = `${post.title}`;
+
+                    divPostCard.appendChild(pPostTitle);
+                    divPostTitle.append(divPostCard, aPosts);
+
+                });
+            });
+        let aPosts = document.createElement('a');
+        aPosts.innerText = 'Post Details';
+        aPosts.href = 'post-details.html';
+
+        divUserButton.append(btnUsers);
+        divPost.append(divUserButton, divPostTitle);
+        divBottomAddress.appendChild(ulAddress);
+        divUserBottom.append(divBottomAddress, divBottomGeo, divBottomCompany);
+        divUserTop.append(divUserTopLeft, divUserTopRight);
+        divUserContainer.append(divUserTop, divUserBottom, divPost);
+        document.body.appendChild(divUserContainer);
+
+    });
